6c82ac766e985c2a8a2676cb6264d9eb425f135e
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-04-24 14:07:39 +0700

    update : komite medis model

--- app/Models/RsiaBerkasKomiteMedis.php
+++ app/Models/RsiaBerkasKomiteMedis.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Thiagoprz\CompositeKey\HasCompositeKey;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class RsiaBerkasKomiteMedis extends Model
 {
     use HasFactory;
+
+    use HasFactory, HasCompositeKey;
+
+    protected $table = 'rsia_berkas_komite_medis';
+
+    protected $primaryKey = ['nomor', 'tgl_terbit'];
+
+    protected $guarded = [];
+
+
+    public function penanggungJawab()
+    {
+        return $this->belongsTo(Pegawai::class, 'pj', 'nik')->select('nik', 'nama');
+    }
 }
