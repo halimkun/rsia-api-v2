08981bcc8a741c65dba3e53fe9586928af0ea88e
Faisal Halim <ffaisalhalim@gmail.com>
2024-06-19 08:53:48 +0700

    Update : middleware in registrasi.php to include 'dokter' role

--- routes/partials/registrasi.php
+++ routes/partials/registrasi.php
@@ -3,7 +3,7 @@
 use Orion\Facades\Orion;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['claim:role,pegawai,pasien'])->group(function ($router) {
+Route::middleware(['claim:role,pegawai|pasien|dokter'])->group(function ($router) {
     Route::prefix('booking')->group(function ($router) {
         Orion::resource('registrasi', \App\Http\Controllers\Orion\BookingRegistrasiController::class)->only(['search']);
         Route::apiResource('registrasi', \App\Http\Controllers\v2\BookingRegistrasiController::class)->only(['store', 'show', 'update', 'destroy']);
