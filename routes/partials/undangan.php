d2d4cd4d4e598da22b5cbe864b34724204384e36
Faisal Halim <ffaisalhalim@gmail.com>
2024-06-26 13:30:06 +0700

    Update : undangan.php routes and controllers

--- routes/partials/undangan.php
+++ routes/partials/undangan.php
@@ -3,20 +3,19 @@
 use Orion\Facades\Orion;
 use Illuminate\Support\Facades\Route;
 
-Orion::resource('undangan', \App\Http\Controllers\Orion\RsiaUndangan::class)
-    // ->middleware(['user-aes', 'claim:role,pegawai'])
-    ->only('search');
+Orion::resource('undangan', \App\Http\Controllers\Orion\RsiaUndanganController::class)
+->only('search'); // INFO : selesai
 
+// FIXME : review ulang endpoint undangan (SEMUA) 
 Route::middleware(['user-aes', 'claim:role,pegawai'])->prefix('undangan')->group(function () {
     // ==================== PENERIMA UNDANGAN 
     Orion::resource('penerima', \App\Http\Controllers\Orion\RsiaPenerimaUndanganController::class)->only(['search'])
-        ->parameters(['penerima' => 'base64_no_surat']);
+        ->parameters(['penerima' => 'base64_no_surat']); // INFO : selesai
         
     Route::apiResource('penerima', \App\Http\Controllers\v2\RsiaPenerimaUndanganController::class)
         ->only(['store', 'show'])
         ->parameters(['penerima' => 'base64_no_surat']);
 
-    // FIXME : review ulang endpoint undangan (SEMUA)
     // ==================== KEHADIRAN RAPAT
     Route::apiResource('kehadiran', \App\Http\Controllers\v2\RsiaKehadiranRapatController::class)
         ->only(['store', 'show'])
