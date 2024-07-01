26c69b5ebc7657a4376db4d4d16036f1d9bbf384
Faisal Halim <ffaisalhalim@gmail.com>
2024-07-01 08:53:39 +0700

    :bug: fix: restructuring routes
    Restrukturisasi urutan route untuk mengatasi konflik dan meningkatkan aksesibilitas endpoint API.

--- routes/partials/pasien.php
+++ routes/partials/pasien.php
@@ -5,58 +5,51 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['claim:role,pegawai|dokter|pasien'])->prefix('pasien')->group(function () {
 
-    // ==================== PASIEN
-    Orion::resource('.', \App\Http\Controllers\Orion\PasienController::class)->only('search')->parameters(['pasien' => 'no_rkm_medis']);
-    Route::apiResource('.', \App\Http\Controllers\v2\PasienController::class)->parameters(['' => 'no_rkm_medis']);
-
     // ==================== RIWAYAT PEMERIKSAAN PASIEN
     Route::apiResource('.riwayat', \App\Http\Controllers\v2\RiwayatPemeriksaanPasienController::class)
         ->parameters(['' => 'no_rkm_medis', 'riwayat' => 'no_rawat'])->only(['index', 'show']);
-    
+
     // ==================== RIWAYAT PEMERIKSAAN PASIEN CUSTOM ROUTE
     Route::apiResource('.riwayat.ralan', \App\Http\Controllers\v2\RiwayatPemeriksaanRalan::class)
         ->parameters(['' => 'no_rkm_medis', 'riwayat' => 'no_rawat'])->only(['index']);
-    
+
     Route::apiResource('.riwayat.ranap', \App\Http\Controllers\v2\RiwayatPemeriksaanRanap::class)
         ->parameters(['' => 'no_rkm_medis', 'riwayat' => 'no_rawat'])->only(['index']);
-    
+
     Route::apiResource('.riwayat.lab', \App\Http\Controllers\v2\RiwayatPeriksaLabController::class)
         ->parameters(['' => 'no_rkm_medis', 'riwayat' => 'no_rawat'])->only(['index']);
-    
+
     Route::apiResource('.riwayat.radiologi', \App\Http\Controllers\v2\RiwayatPeriksaRadiologiController::class)
         ->parameters(['' => 'no_rkm_medis', 'riwayat' => 'no_rawat'])->only(['index']);
-    
+
     Route::apiResource('.riwayat.obat', \App\Http\Controllers\v2\RiwayatPemberianObatController::class)
         ->parameters(['' => 'no_rkm_medis', 'riwayat' => 'no_rawat'])->only(['index']);
 
 
     Route::middleware(['user-aes'])->group(function () {
         // ==================== PASIEN RAWAT INAP
+        Route::resource('ranap', \App\Http\Controllers\v2\PasienRawatInapController::class)->only('index');
         Orion::resource('ranap', \App\Http\Controllers\Orion\PasienRawatInapController::class)->only('search')
-            ->parameters(['ranap' => 'base64_no_rawat']);
-        Route::apiResource('ranap', \App\Http\Controllers\v2\PasienRawatInapController::class)->only('index')
             ->parameters(['ranap' => 'base64_no_rawat']);
 
         // ==================== PASIEN RAWAT INAP CUSTOM ROUTE
-        Route::post('ranap/real-cost', [\App\Http\Controllers\v2\RealCostController::class, 'ranap'])
-            ->name('ranap.real-cost');
-        Route::post('ranap/grouping-cost', [\App\Http\Controllers\v2\GroupingCostController::class, 'ranap'])
-            ->name('ranap.grouping-cost');
+        Route::post('ranap/real-cost', [\App\Http\Controllers\v2\RealCostController::class, 'ranap'])->name('ranap.real-cost');
+        Route::post('ranap/grouping-cost', [\App\Http\Controllers\v2\GroupingCostController::class, 'ranap'])->name('ranap.grouping-cost');
 
         // ==================== TARIF PASIEN RAWAT INAP (REAL COST UNTUK SPESIFIK PASIEN RAWAT INAP)
-        Route::apiResource('ranap.real-cost', \App\Http\Controllers\v2\RealCostPasienRawatInap::class)->only('index')
-            ->parameters(['ranap' => 'base64_no_rawat']);
+        Route::apiResource('ranap.real-cost', \App\Http\Controllers\v2\RealCostPasienRawatInap::class)->only('index')->parameters(['ranap' => 'base64_no_rawat']);
 
         // ==================== BILLING PASIEN RAWAT INAP
-        Route::apiResource('ranap.billing', \App\Http\Controllers\v2\BillingPasienController::class)->only('index')
-            ->parameters(['ranap' => 'base64_no_rawat']);
-
+        Route::apiResource('ranap.billing', \App\Http\Controllers\v2\BillingPasienController::class)->only('index')->parameters(['ranap' => 'base64_no_rawat']);
 
 
         // ==================== PASIEN RAWAT JALAN
-        Orion::resource('ralan', \App\Http\Controllers\Orion\PasienRawatJalanController::class)->only('search')
-            ->parameters(['ralan' => 'base64_no_rawat']);
-        Route::apiResource('ralan', \App\Http\Controllers\v2\PasienRawatJalanController::class)->only('index')
-            ->parameters(['ralan' => 'base64_no_rawat']);
+        Orion::resource('ralan', \App\Http\Controllers\Orion\PasienRawatJalanController::class)->only('search')->parameters(['ralan' => 'base64_no_rawat']);
+        Route::apiResource('ralan', \App\Http\Controllers\v2\PasienRawatJalanController::class)->only('index')->parameters(['ralan' => 'base64_no_rawat']);
     });
+
+    // ==================== PASIEN
+    Route::apiResource('/', \App\Http\Controllers\v2\PasienController::class)->parameters(['' => 'no_rkm_medis']);
+    Orion::resource('', \App\Http\Controllers\Orion\PasienController::class)->only('search')->parameters(['pasien' => 'no_rkm_medis']);
+
 });
