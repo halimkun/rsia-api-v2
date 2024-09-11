6b1cc6a655de30a47d7637d302d69f81093a3c18
Faisal Halim <ffaisalhalim@gmail.com>
2024-09-11 11:29:18 +0700

    :hammer: update (pdf) : update print layout

--- resources/views/layouts/print.blade.php
+++ resources/views/layouts/print.blade.php
@@ -11,21 +11,9 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/print.css') }}">
 
-    <style>
-        .header-print {
-            position: fixed;
-            top: -20px;
-            left: 0px;
-            right: 0px;
-            height: 50px;
+    @stack('styles')
 
-            /** Extra personal styles **/
-            background-color: #03a9f4;
-            color: white;
-            text-align: center;
-            line-height: 35px;
-        }
-
+    {{-- <style>
         @page {
             /* meaning top, right, bottom, left */
             margin: 100px 50px 50px 50pxp; /* Default margins for all pages */
@@ -37,17 +25,14 @@
 
         header { position: fixed; top: -10px; left: 0px; right: 0px; height: 50px; max-height: min-content !important; }
         footer { position: fixed; bottom: -60px; left: 0px; right: 0px; background-color: lightblue; height: 50px; }
-    </style>
+    </style> --}}
+    
 </head>
 
 <body class="font-serif antialiased">
-    <header>
-        @stack('header')
-    </header>
+    @stack('header')
     
-    <main class="px-3" style="margin-top: 50px !important;">
-        {{ $slot }}
-    </main>
+    {{ $slot }}
 </body>
 
 </html>
