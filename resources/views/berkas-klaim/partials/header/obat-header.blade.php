9bafe08be1dec90543e9e7dbf46fbe08cb933177
Faisal Halim <ffaisalhalim@gmail.com>
2024-09-25 09:12:50 +0700

    :hammer: update (obat views) : refactor header and footer structure; replace <main> with <div> for consistency and add new HTML partial for header

--- resources/views/berkas-klaim/partials/header/obat-header.blade.php
+++ resources/views/berkas-klaim/partials/header/obat-header.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <header style="width: 100%;">
+    <header style="width: 100%; padding: 0 0; margin: 0 0;">
         <table style="border-bottom: 1px solid #333; width: 100%">
             <tr style="text-align: center;">
                 <td style="text-align: center;">
@@ -22,7 +12,7 @@
         </table>
     </header>
 
-    <main>
+    <div style="padding: 0 0; margin: 0 0;">
         <div style="width: 100%;">
             <table style="width: 100%; border-bottom: 1px solid #333;">
                 @foreach ([
