aed6b92850c79c391bea3c62ffc2c6a091817058
Faisal Halim <ffaisalhalim@gmail.com>
2024-09-25 13:47:48 +0700

    :hammer: config (index.php) : increase max execution time to 5 minutes for improved performance on long-running processes

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+ini_set('max_execution_time', '300'); //300 seconds = 5 minutes
+
 define('LARAVEL_START', microtime(true));
 
 /*
