e11bc1a1afae83e3075839da53d5c037a8358947
Faisal Halim <ffaisalhalim@gmail.com>
2024-09-30 09:31:23 +0700

    :recycle: refactor : print method in BerkasKlaimController.php

--- resources/views/berkas-klaim/partials/laporan-operasi.blade.php
+++ resources/views/berkas-klaim/partials/laporan-operasi.blade.php
@@ -87,95 +87,120 @@
                 <tr>
                     <td class="border px-1 align-top" style="border: 1px solid #333;" colspan="2">
                         <p class="font-bold">Dokter Ahli Bedah :</p>
-                        <p class="text-nowrap whitespace-nowrap">{{ $operasi->detailOperator1->nm_dokter }}</p>
+                        <p class="text-nowrap whitespace-nowrap">{{ $operasi?->detailOperator1?->nm_dokter }}</p>
                     </td>
                     <td class="border px-1 align-top" style="border: 1px solid #333;">
                         <p class="font-bold">Asisten :</p>
-                        <p class="text-nowrap whitespace-nowrap">{{ $operasi->detailAsistenOperator1->nama }}</p>
+                        <p class="text-nowrap whitespace-nowrap">{{ $operasi?->detailAsistenOperator1?->nama }}</p>
                     </td>
                     <td class="border px-1 align-top" style="border: 1px solid #333;">
                         <p class="font-bold">Perawat :</p>
-                        <p class="text-nowrap whitespace-nowrap">{{ $operasi->detailAsistenOperator2->nama }}</p>
+                        <p class="text-nowrap whitespace-nowrap">{{ $operasi?->detailAsistenOperator2?->nama }}</p>
                     </td>
                 </tr>
                 <tr>
                     <td class="border px-1 align-top" style="border: 1px solid #333;" colspan="2">
                         <p class="font-bold">Dokter Ahli Anestesi :</p>
-                        <p>{{ $operasi->detailDokterAnestesi->nm_dokter }}</p>
+                        <p>{{ $operasi?->detailDokterAnestesi?->nm_dokter }}</p>
                     </td>
                     <td class="border px-1 align-top" style="border: 1px solid #333;" colspan="2">
                         <p class="font-bold">Jenis Anestesi :</p>
-                        <p>{{ $operasi->jenis_anestesi }}</p>
+                        <p>{{ $operasi?->jenis_anestesi }}</p>
                     </td>
                 </tr>
                 <tr>
                     <td class="border px-1 align-top" style="border: 1px solid #333;" colspan="2">
                         <p class="font-bold">Diagnosa Pre Operatif :</p>
-                        <p>{{ $operasi->diagnosa_preop }}</p>
+                        <p>{{ $operasi?->diagnosa_preop }}</p>
                     </td>
                     <td class="border px-1 align-top" style="border: 1px solid #333;" colspan="2">
                         <p class="font-bold">Diagnosa Post Operatif :</p>
-                        <p>{{ $operasi->diagnosa_postop }}</p>
+                        <p>{{ $operasi?->diagnosa_postop }}</p>
                     </td>
                 </tr>
                 <tr>
                     <td class="border px-1 align-top" style="border: 1px solid #333;" colspan="2">
                         <p class="font-bold">Jumlah darah masuk transfusi :</p>
                         <ul class="ml-4">
-                            <li>{{ $operasi->darah_masuk }}</li>
+                            <li>{{ $operasi?->darah_masuk }}</li>
                         </ul>
 
                         <p class="font-bold">Jumlah darah yang hilang :</p>
                         <ul class="ml-4">
-                            <li>{{ $operasi->darah_hilang }}</li>
+                            <li>{{ $operasi?->darah_hilang }}</li>
                         </ul>
                     </td>
                     <td class="border px-1 align-top" style="border: 1px solid #333;" colspan="2">
                         <p class="font-bold">Komplikasi :</p>
-                        <p>{{ $operasi->komplikasi }}</p>
+                        <p>{{ $operasi?->komplikasi }}</p>
                     </td>
                 </tr>
                 <tr>
                     <td class="border px-1 align-top" style="border: 1px solid #333;" colspan="2">
                         <p class="font-bold">Jaringan yang di-Eksis/insisi :</p>
-                        <p>{{ $operasi->jaringan_insisi }}</p>
+                        <p>{{ $operasi?->jaringan_insisi }}</p>
                     </td>
                     <td class="border px-1 align-top" style="border: 1px solid #333;" colspan="2">
                         <p class="font-bold">Pemeriksa PA :</p>
-                        <p>{{ $operasi->pemeriksaan_pa }}</p>
+                        <p>{{ $operasi?->pemeriksaan_pa }}</p>
                     </td>
                 </tr>
                 <tr>
                     <td class="border px-1 align-top" style="border: 1px solid #333;">
                         <p class="font-bold">Tanggal Operasi :</p>
-                        <p>{{ explode(' ', $operasi->tgl_operasi)[0] }}</p>
+                        <p>{{ explode(' ', $operasi?->tgl_operasi)[0] }}</p>
                     </td>
                     <td class="border px-1 align-top" style="border: 1px solid #333;">
                         <p class="font-bold">Jam Mulai Operasi :</p>
-                        <p>{{ explode(' ', $operasi->tgl_operasi)[1] }}</p>
+                        <p>{{ explode(' ', $operasi?->tgl_operasi)[1] }}</p>
                     </td>
                     <td class="border px-1 align-top" style="border: 1px solid #333;">
                         <p class="font-bold">Jam Selesai Operasi :</p>
-                        <p>{{ explode(' ', $operasi->tgl_selesai)[0] }}</p>
+                        <p>{{ explode(' ', $operasi?->tgl_selesai)[1] }}</p>
                     </td>
                     <td class="border px-1 align-top" style="border: 1px solid #333;">
                         <p class="font-bold">Lama Operasi :</p>
-                        <p>{{ explode(' ', $operasi->tgl_selesai)[1] }}</p>
+                        @php
+                            $start = new DateTime($operasi?->tgl_operasi);
+                            $end = new DateTime($operasi?->tgl_selesai);
+                            $diff = $start?->diff($end);
+                        @endphp
+
+                        @if ($diff?->h > 0)
+                            <p>{{ $diff?->format('%H jam %i menit %s detik') }}</p>
+                        @else
+                            <p>{{ $diff?->format('%i menit %s detik') }}</p>
+                        @endif
                     </td>
                 </tr>
                 <tr>
                     <td class="border px-1 align-top" style="border: 1px solid #333;" colspan="4">
                         <p class="font-bold">Nama/ Macam Operasi :</p>
-                        <p>{{ $operasi->detailPaket->nm_perawatan }}</p>
+                        <p>{{ $operasi?->detailPaket?->nm_perawatan }}</p>
                     </td>
                 </tr>
                 <tr>
                     <td class="border px-1 align-top" style="border: 1px solid #333;" colspan="4">
-                        <p class="font-bold mb-3">Laporan Jalannya Operasi :</p>
-                        <p class="leading-4">{!! nl2br($operasi->laporan_operasi) !!}</p>
+                        <p class="mb-3 font-bold">Laporan Jalannya Operasi :</p>
+                        <p class="leading-4">{!! nl2br($operasi?->laporan_operasi) !!}</p>
                     </td>
                 </tr>
             </table>
+
+            <div>
+                <div class="float-right mt-8 text-center">
+                    @php
+                        $QRDokter = 'Dikeluarkan di RSIA Aisyiyah Pekajangan, Ditandatangani secara elektronik oleh ' . $operasi?->detailOperator1?->nm_dokter . '. ID : ' . $operasi?->detailOperator1?->sidikjari?->sidikjari;
+                    @endphp
+
+                    <div class="relative inline-block h-28 w-28">
+                        <img src="data:image/png;base64,{{ DNS2D::getBarcodePNG($QRDokter, 'QRCODE') }}" alt="barcode" class="h-28 w-28" />
+                        <img src="{{ asset('assets/images/logo.png') }}" alt="logo" class="h-8 w-8" style="position: absolute !important; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 10;" />
+                    </div>
+
+                    <p>{{ $operasi?->detailOperator1?->nm_dokter }}</p>
+                </div>
+            </div>
         </main>
     @endpush
 </x-print-layout>
